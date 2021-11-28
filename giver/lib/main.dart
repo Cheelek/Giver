2a5c6df83c84a2e3bf829d60532f50f0fcb1f79b
512143 <cheelle@vk.com>
2021-11-28 09:42:28 +0300

    a

---
+++ giver/lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  const App({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+   return MaterialApp(
+     home: PageOne(),
+     
+   );
+  }
+}
+
+class PageOne extends StatefulWidget {
+  PageOne({Key key}) : super(key: key);
+
+  @override
+  _PageOneState createState() => _PageOneState();
+}
+
+class _PageOneState extends State<PageOne> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ,
+    );
+  }
+}
